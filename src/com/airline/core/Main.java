6125b9b96471675dcf8976ba4da58cd6f5846d12
U <devvevan@gmail.com>
2021-08-03 20:03:16 +0700

    added main menu

--- src/com/airline/core/Main.java
+++ src/com/airline/core/Main.java
@@ -2,6 +2,15 @@ package com.airline.core;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+
+        System.out.println("AIRLINE RESERVATION");
+        System.out.println("=================");
+
+        System.out.println("1. Booking pesawat\n" +
+                "2. Cek Tiket Tersedia\n" +
+                "3. Cancel Tiket\n" +
+                "4. Ganti Jadwal\n" +
+                "\n" +
+                "pilih menu:");
     }
 }
