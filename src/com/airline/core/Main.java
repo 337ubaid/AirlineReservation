1cd8453ef583f935ad062331cf09272cc76f52b3
Sulthan Abiyyu <zblackjack980@gmail.com>
2021-08-03 22:33:40 +0700

    Added booking and transaction logic

--- src/com/airline/core/Main.java
+++ src/com/airline/core/Main.java
@@ -39,3 +39,16 @@ public class Main {
         }
     }
 }
+
+class User{
+    private int saldo;
+
+    User(int saldo){
+        this.saldo=saldo;
+    }
+
+    int getSaldo(){
+        return saldo;
+    }
+
+}
