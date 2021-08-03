ba4af0cecd8ea8245b18bbfd69145ad9d7981aff
Sulthan Abiyyu <zblackjack980@gmail.com>
2021-08-03 20:57:42 +0700

    Added bookingPesawat.java

--- src/com/airline/core/bookingPesawat.java
+++ src/com/airline/core/bookingPesawat.java
@@ -2,7 +2,10 @@ package com.airline.core;
 
 public class bookingPesawat {
     static void viewBookingPesawat(){
-
+        int i = 1;
+        for(String list: dummyList()){
+            System.out.println(i+". " + list);
+        }
     }
 
     static String[] dummyList(){
