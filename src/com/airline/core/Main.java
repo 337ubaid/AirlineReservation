6927593ccc70fb7f1d5c6bc52daf0ec9d601425b
U <devvevan@gmail.com>
2021-08-03 20:13:39 +0700

    update main menu

--- src/com/airline/core/Main.java
+++ src/com/airline/core/Main.java
@@ -1,16 +1,21 @@
 package com.airline.core;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("AIRLINE RESERVATION");
-        System.out.println("=================");
+        System.out.println("===================");
 
-        System.out.println("1. Booking pesawat\n" +
+        System.out.print("1. Booking pesawat\n" +
                 "2. Cek Tiket Tersedia\n" +
                 "3. Cancel Tiket\n" +
                 "4. Ganti Jadwal\n" +
                 "\n" +
-                "pilih menu:");
+                "pilih menu: ");
+
+        Scanner inputmenu = new Scanner(System.in);
+        int pilih = inputmenu.nextInt();
     }
 }
